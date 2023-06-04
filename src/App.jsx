23248eb7cc1bb6f93322ae9de1c74b5b1c70fbc1
Matias Altalef <maltalef@gmail.com>
2023-06-04 23:20:06 +0000

    foobaar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <hgroup>
+        <article>
+          <img src="images/group.svg" />
+          <h1>Meet the <i style={{color:"SteelBlue"}}>StarGazers</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={ () => alert("HIII") }>Click Me</button>
+        </article>
+      </hgroup>
+    </div>
   )
 }
 export default App
